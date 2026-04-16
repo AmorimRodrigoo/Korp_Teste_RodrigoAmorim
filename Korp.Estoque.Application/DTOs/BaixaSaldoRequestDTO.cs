a81119db40af8728988c423e7e29c90082296895
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2026-04-15 23:49:52 -0300

    test

---
+++ Korp.Estoque.Application/DTOs/BaixaSaldoRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Korp.Estoque.Application.DTOs;
+
+public class BaixaSaldoRequestDTO
+{
+    [Required]
+    public string Codigo { get; set; } = string.Empty;
+    
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero.")]
+    public int Quantidade { get; set; }
+}
